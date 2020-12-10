7ef38348ae43fe0431410d24acbbafb45d786ac7
NGUYEN CHI CONG <congnc.if@gmail.com>
2020-12-10 13:14:52 +0700

    Init Store Reference Manager

---
+++ ReferenceStoreManager.podspec
@@ -0,0 +1,25 @@
+Pod::Spec.new do |s|
+  s.name             = 'ReferenceStoreManager'
+  s.version          = '0.1.0'
+  s.swift_versions   = ["5.0", "5.1", "5.2", "5.3"]
+  s.summary          = 'A testable approach for global state.'
+
+  s.description      = <<-DESC
+* Prevent to access directly to a singleton which will be hard to test.
+* Avoid to pass a short-lifetime states across some objects that can make your logic more complex.
+                       DESC
+
+  s.homepage         = 'https://github.com/congncif/RxReferenceStoreManager'
+  # s.screenshots     = 'www.example.com/screenshots_1', 'www.example.com/screenshots_2'
+  s.license          = { :type => 'MIT', :file => 'LICENSE' }
+  s.author           = { 'CONG NGUYEN CHI' => 'congnc.if@gmail.com' }
+  s.source           = { :git => 'https://github.com/congncif/RxReferenceStoreManager.git', :tag => s.version.to_s }
+   s.social_media_url = 'https://twitter.com/congncif'
+
+  s.ios.deployment_target = '9.0'
+
+  s.source_files = 'Sources/*.swift'
+  
+  s.dependency 'RxSwift'
+  s.dependency 'RxCocoa'
+end
---
+++ Sources/ReferenceStoreManager.swift
@@ -0,0 +1,161 @@
+//
+//  ReferenceStoreManager.swift
+//  Boardy
+//
+//  Created by NGUYEN CHI CONG on 9/15/20.
+//
+
+import Foundation
+import RxCocoa
+import RxSwift
+
+// MARK: - ObjectReferenceStorable
+
+public final class ReferenceStoreManager {
+    private var storage: [AnyHashable: [AnyObject]] = [:]
+    private var disposeBags: [AnyHashable: DisposeBag] = [:]
+
+    public init() {}
+
+    public static let shared = ReferenceStoreManager()
+
+    public func storeObject<Object>(_ object: AnyObject, untilObjectKilled pairObject: Object) where Object: ReactiveCompatible, Object: AnyObject {
+        let key = ObjectIdentifier(pairObject)
+
+        if var objects = storage[key] {
+            if objects.contains(where: { $0 === object }) {
+                return
+            }
+            objects.append(object)
+            storage[key] = objects
+        } else {
+            storage[key] = [object]
+        }
+
+        if disposeBags[key] != nil { return }
+
+        let disposeBag = DisposeBag()
+        disposeBags[key] = disposeBag
+
+        pairObject.rx.deallocated
+            .subscribe(onNext: { [weak self] in
+                self?.storage.removeValue(forKey: key)
+                self?.disposeBags.removeValue(forKey: key)
+            })
+            .disposed(by: disposeBag)
+    }
+
+    public func storedObjects(pairedWith object: AnyObject) -> [AnyObject] {
+        let key = ObjectIdentifier(object)
+        return storage[key] ?? []
+    }
+
+    public func storeObject<Object: AnyObject>(_ object: Object, forKey key: AnyHashable = String(describing: Object.self)) {
+        #if DEBUG
+        if let currentObject: Object = storedObject(forKey: key) {
+            print("⚠️ Store key should be unique. Object \(currentObject) with key \(key) was stored before. Store many times with same key will remove stored object.")
+        }
+        #endif
+        storage[key] = [object]
+    }
+
+    public func storedObject<Object: AnyObject>(forKey key: AnyHashable = String(describing: Object.self)) -> Object? {
+        storage[key]?.compactMap { $0 as? Object }.first
+    }
+
+    public func removeObject(forKey key: AnyHashable) {
+        storage.removeValue(forKey: key)
+    }
+
+    public func removeObject<Object: AnyObject>(_ objectType: Object.Type) {
+        removeObject(forKey: String(describing: Object.self))
+    }
+}
+
+public protocol PairableObject: AnyObject {
+    var referenceStoreManager: ReferenceStoreManager { get }
+
+    func pairWith<Object>(object: Object) where Object: ReactiveCompatible, Object: AnyObject
+}
+
+extension PairableObject {
+    public var referenceStoreManager: ReferenceStoreManager { .shared }
+
+    public func pairWith<Object>(object: Object) where Object: ReactiveCompatible, Object: AnyObject {
+        referenceStoreManager.storeObject(self, untilObjectKilled: object)
+    }
+
+    public func pairedObjects<Object>() -> [Object] {
+        referenceStoreManager.storedObjects(pairedWith: self).compactMap {
+            $0 as? Object
+        }
+    }
+
+    public func pairedObject<Object>() -> Object? {
+        pairedObjects().first
+    }
+}
+
+// MARK: - SelfStorableObject
+
+public protocol SelfStorableObject: AnyObject {
+    func keepMe()
+    func dropMe()
+}
+
+public extension SelfStorableObject {
+    private var referenceStoreManager: ReferenceStoreManager { .shared }
+
+    func keepMe() {
+        referenceStoreManager.storeObject(self, forKey: ObjectIdentifier(self))
+    }
+
+    func dropMe() {
+        referenceStoreManager.removeObject(forKey: ObjectIdentifier(self))
+    }
+}
+
+// MARK: - ReactiveDisposableObject
+
+public final class DisposeContainer {
+    private var disposeBags: [AnyHashable: DisposeBag] = [:]
+
+    public static let shared = DisposeContainer()
+
+    public func generateDisposeBag<Object>(for object: Object) -> DisposeBag where Object: ReactiveCompatible, Object: AnyObject {
+        let key = ObjectIdentifier(object)
+        if let disposeBag = disposeBags[key] {
+            return disposeBag
+        }
+
+        let disposeBag = DisposeBag()
+        disposeBags[key] = disposeBag
+
+        object.rx.deallocated
+            .subscribe(onNext: { [weak self] in
+                self?.disposeBags.removeValue(forKey: key)
+            })
+            .disposed(by: disposeBag)
+
+        return disposeBag
+    }
+}
+
+public protocol ReactiveDisposableObject: AnyObject {
+    var disposeContainer: DisposeContainer { get }
+    var freshDisposeBag: DisposeBag { get }
+}
+
+extension ReactiveDisposableObject where Self: ReactiveCompatible {
+    public var disposeContainer: DisposeContainer { .shared }
+
+    public var freshDisposeBag: DisposeBag {
+        disposeContainer.generateDisposeBag(for: self)
+    }
+}
+
+// MARK: - Utility Extensions
+
+extension NSObject: PairableObject {}
+extension NSObject: ReactiveDisposableObject {}
+extension NSObject: SelfStorableObject {}
